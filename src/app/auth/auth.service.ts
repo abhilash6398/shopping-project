07241094c266583fdad6d42066e224a2b36a8039
abhilash6398 <abhilash6398@gmail.com>
2021-06-13 16:23:01 +0530

    Updated-error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
   kind: string;
@@ -19,10 +21,20 @@ export class AuthService {
 
   signup(email:string,password:string){
     return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBATyfh56zuK-fUoY3d7veTIiZ9jEi79bc',
-    {   
+    {
       email: email,
       password: password,
       returnSecureToken: true
-    });
+    }).pipe(catchError(errorRes => {
+      let errorMessage='An unknown error occured';
+      if(!errorRes.error || !errorRes.error.error){
+        return throwError(errorMessage);
+      }
+      switch(errorRes.error.error.message){
+        case 'EMAIL EXISTS':
+          errorMessage='this email already exists';
+      }
+      return throwError(errorMessage);
+    }));
   }
 }
--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -10,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class AuthComponent {
 
   isLoginMode = true;
+  isLoading= false;
+  error: string =null;
 
   constructor(private authService: AuthService){}
 
@@ -23,13 +25,18 @@ export class AuthComponent {
     }
     const email=form.value.email;
     const password= form.value.password;
+
+    this.isLoading=true;
     if(this.isLoginMode){
       //..
     } else {
       this.authService.signup(email,password).subscribe(resdata=>{
         console.log(resdata);
-      },error=>{
-        console.log(error);
+        this.isLoading=false;
+      },errorMessage=>{
+        console.log(errorMessage);
+        this.error=errorMessage;
+        this.isLoading=false;
       });
     }
     form.reset();
